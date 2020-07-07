5ad4f26c46a65b1b218921c621b8f96cb923d775
Roman <roma@mail.ru>
2020-07-07 19:01:01 +0300

    scrollSecond

--- js/main.js
+++ js/main.js
@@ -10,24 +10,7 @@
 //     })
 // }
 
-// $(document).ready( function() {
-//     let anchors = $('.header-menu__links');
-//     for (anchor of anchors) {
-//         anchor.click (function(event) {
-//             event.preventDefault();
-//             let anchorId = anchor.attr('href');
-//             console.log (anchorId);
-//             let top = anchorId.offset().top;
-//             anchorId.animate({
-//                 scrollTop: top
-//             }, 5000)
-//         })  
-//     }
-    
-// })
 $(document).ready( function() {
-    hed = $('header').height();
-    console.log (hed);
     $('.header-menu__links').click (function(e) {
         e.preventDefault();
         let anchorId = $(this).attr('href');
